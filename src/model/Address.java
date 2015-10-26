54886b31e37a5e027dfb28f6b4a14cdde7b1aae3
thunder-tw <wardtruyen@gmail.com>
2015-10-26 18:56:28 +0100

    Basis structuur project
    Basis structuur project opgezet t.a.v. opracht 3 (SportsTracker)

---
+++ src/model/Address.java
@@ -0,0 +1,154 @@
+package model;
+
+public class Address {
+	private String street;
+	private String number;
+	private String box;
+	private String zipCode;
+	private String city;
+	private String phone;
+	private String mobilePhone;
+	private String email;
+	private String fax;
+
+	/**
+	 * @return the street
+	 */
+	public String getStreet() {
+		return street;
+	}
+
+	/**
+	 * @param street
+	 *            the street to set
+	 */
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	/**
+	 * @return the number
+	 */
+	public String getNumber() {
+		return number;
+	}
+
+	/**
+	 * @param number
+	 *            the number to set
+	 */
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	/**
+	 * @return the box
+	 */
+	public String getBox() {
+		return box;
+	}
+
+	/**
+	 * @param box
+	 *            the box to set
+	 */
+	public void setBox(String box) {
+		this.box = box;
+	}
+
+	/**
+	 * @return the zipCode
+	 */
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	/**
+	 * @param zipCode
+	 *            the zipCode to set
+	 */
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+
+	/**
+	 * @param city
+	 *            the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	/**
+	 * @return the phone
+	 */
+	public String getPhone() {
+		return phone;
+	}
+
+	/**
+	 * @param phone
+	 *            the phone to set
+	 */
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	/**
+	 * @return the mobilePhone
+	 */
+	public String getMobilePhone() {
+		return mobilePhone;
+	}
+
+	/**
+	 * @param mobilePhone
+	 *            the mobilePhone to set
+	 */
+	public void setMobilePhone(String mobilePhone) {
+		this.mobilePhone = mobilePhone;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email
+	 *            the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the fax
+	 */
+	public String getFax() {
+		return fax;
+	}
+
+	/**
+	 * @param fax
+	 *            the fax to set
+	 */
+	public void setFax(String fax) {
+		this.fax = fax;
+	}
+
+	public String toString() {
+		return String.format("%s %s bus %s\n%s %s\n%s\nTel.: %s\nGsm.: %s\nFax: %s", getStreet(), getNumber(), getBox(),
+				getZipCode(), getCity(), getEmail(), getPhone(),
+				getMobilePhone(), getFax());
+	}
+}
---
+++ src/model/Training.java
@@ -0,0 +1,20 @@
+/**
+ * @Autor: Ward Truyen, Ben Vanden Dries
+ * @Team: Team08
+ * @Date: 10/26/2015
+ * @Project: SportsTracker
+ * @Purpose: gegevens van een Training beheren 
+ */
+
+package model;
+
+import java.util.GregorianCalendar;
+
+public class Training {
+	private
+		Address adres; // waar de training afspeelde
+		GregorianCalendar datum; // wanneer de training afspeelde
+		double afstand; // afgelegde afstand in km, tijdens de training
+		long tijdInSec; // gebruikte tijd voor de training in sec
+		ESportType type;
+}
